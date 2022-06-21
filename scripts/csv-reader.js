da48070e06736d2c203248de143157e5e02d5947
Danilo Packer <danilo.kpacker@gmail.com>
2022-06-21 19:27:16 +0000

    commit

--- scripts/csv-reader.js
+++ scripts/csv-reader.js
@@ -1,17 +1,27 @@
 const csvFile = document.getElementById("csvFile").files[0]; //seleciona o arquivo csv
 var btnEnviarCsv = document.getElementById("send-csv") //seleciona o botão que fará a ação desejada
-var a = [];
-
+nome = [];
+var desconto = [];
+funcionario = {}; 
 btnEnviarCsv.addEventListener("click",function(e){ //escuta a ação de clich no botão e executa a função de ler o arquivo csv e transformar em um objeto.
     e.preventDefault();
     Papa.parse(csvFile, {
         header: true,
         complete: function(results) {
             console.log("Finished:", results.data);
-            results.data.forEach(element => {
-                console.log('a')
-            });
+            results.data.forEach(Imprimir)
+            results.data.forEach(PegaFaltas)
         }
     });
+    console.log(nome)
+    console.log('oii' + funcionario)
 });
 
+function Imprimir (item,indice){
+    nome[indice] = item.Nome;
+}
+
+function PegaFaltas (item,index){
+    let desconto = item.map(Matrícula => Matrícula.foo);
+    console.log(desconto);
+}
